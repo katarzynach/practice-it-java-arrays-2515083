5c440b623f737a7ffda7019750b8346020e64da6
katarzynach <kasia97h@o2.pl>
2023-05-18 14:25:22 +0200

    printTriangle

--- src/App.java
+++ src/App.java
@@ -1,8 +1,24 @@
+import java.util.Arrays;
+
 public class App {
   
   public static void printTriangle(Object[] arr) {
-
+        for (int i=0; i<arr.length; i++){
+            Arrays.stream(Arrays.copyOfRange(arr, 0, i+1)).forEach(System.out::print);
+            System.out.println();
+        }
+      System.out.println();
  }
+
+    public static void printTriangle_v2(Object[] arr) {
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col <= row; col++) {
+                System.out.print(arr[col]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
     
   public static void main(String args[]) {
     printTriangle(new Integer[] {1, 2, 3, 4});
