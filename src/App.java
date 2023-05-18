1b9661e0ebb6b4b087a4e821d3803b799584320d
katarzynach <kasia97h@o2.pl>
2023-05-18 15:13:24 +0200

    reverse array

--- src/App.java
+++ src/App.java
@@ -1,11 +1,25 @@
 import java.util.Arrays;
 
 public class App {
-    
+
   public static void reverse(int[] arr) {
-    
+    int index = arr.length - 1;
+    int temp;
+    for (int i = 0; i < arr.length / 2; i++) {
+      temp = arr[i];
+      arr[i] = arr[index];
+      arr[index] = temp;
+      index--;
+    }
   }
-  
+  //public static void reverse(int[] arr) {
+  //    // 4 3 2 1
+  //    for (int i = 0; i < arr.length / 2; i++) {
+  //      int temp = arr[i]; // 2
+  //      arr[i] = arr[arr.length - 1 - i];
+  //      arr[arr.length - i - 1] = temp;
+  //    }
+  //  }
   public static void main(String args[]) {
     int[] arr = new int[] {};
     System.out.println(Arrays.toString(arr));
